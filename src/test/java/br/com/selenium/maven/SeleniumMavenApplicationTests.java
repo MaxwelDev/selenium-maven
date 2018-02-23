11c5acbff682cba00b322532d4204d30bf3d1ebe
Maxwel Lidogério Pereira da Silva <peo_msilva@ci-66834-7.uolcorp.intranet>
2018-02-23 09:50:13 -0300

    working test in maven integration-test fase

--- src/test/java/br/com/selenium/maven/SeleniumMavenApplicationTests.java
+++ src/test/java/br/com/selenium/maven/SeleniumMavenApplicationTests.java
@@ -1,16 +1,31 @@
 package br.com.selenium.maven;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class SeleniumMavenApplicationTests {
+
+	private WebDriver driver;
+
+    @Before
+    public void iniciaNavegador() {
+        System.setProperty("webdriver.chrome.driver", "/home/peo_msilva/Documents/stash/chromedriver");
+    }
 
 	@Test
 	public void contextLoads() {
+        iniciaChrome();
 	}
 
+    private void iniciaChrome() {
+        driver = new ChromeDriver();
+    }
 }

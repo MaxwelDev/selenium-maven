c6dc8cdbc8f018ba7d3c54b34cbc8e9a9a890594
Maxwel Lidogério Pereira da Silva <peo_msilva@ci-66834-7.uolcorp.intranet>
2018-02-23 09:42:57 -0300

    initial settings

---
+++ src/main/java/br/com/selenium/maven/SeleniumMavenApplication.java
@@ -0,0 +1,12 @@
+package br.com.selenium.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumMavenApplication.class, args);
+	}
+}
